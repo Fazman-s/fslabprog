e6d514f7b254a143a0a2a5442560bb5e200752c0
Fazman S <fazmansam@gmail.com>
2023-07-15 08:03:36 +0530

    Update Lab1.cpp

--- Lab1.cpp
+++ Lab1.cpp
@@ -4,59 +4,54 @@ these names spelled in reverse order to the standard output using I/O redirectio
 pipes. Repeat the exercise using an input file specified by the user instead of the
 standard input and using an output file specified by the user instead of the standard
 output.*/
+
 #include<iostream>
-#include<fstream>
+#include<stdlib.h>
 #include<string.h>
+#include<fstream>
 using namespace std;
 
-//Function to reverse the string
-char* strrev(char *str)
-{
-    int i = strlen(str) - 1, j = 0;
-
-    char ch;
-    while (i > j)
-    {
-        ch = str[i];
-        str[i] = str[j];
-        str[j] = ch;
-        i--;
-        j++;
+char *strrev(char *name){
+    int i=0,temp;
+    int l=strlen(name);
+    while(i<l){
+        temp=name[i];
+        name[i]=name[l-1];
+        name[l-1]=temp;
+        i++;
+        l--;
     }
-    return str;
+    return name;
 }
 
-int main()
-{
-	fstream file1,file2;
-	char ifile[10],ofile[10],temp[25];
-	int n,i=0;
-	
-	cout<<"Enter the first filename"<<endl;//Read the name of the input file
-	cin>>ofile;
-	cout<<"Enter the number of names to be entered"<<endl;//Read number of names to be inserted
-	cin>>n;
-	cout<<"Enter the names:"<<endl;
-	file1.open(ofile,ios::out);
-	while(i<n)//Read n names and write to input file
-	{
-		cin>>temp;
-		file1<<temp<<endl;
-		i++;
-	}
-	file1.close();
-	file1.open(ofile,ios::in);//reopen input file in read mode
-	cout<<"Enter the second filename"<<endl;
-	cin>>ifile;
-	file2.open(ifile,ios::out);//Open output file in write mode
-	i=0;
-	while(i<n)//read and reverse the names and write to output file
-	{
-		file1>>temp;
-		file2<<strrev(temp)<<endl;
-		cout<<temp<<endl;
-		i++;
-	}
-	cout<<"Names are reversed and written successfully  :) ";
+int main(){
+    fstream file1,file2;
+    int i=0,n;
+    char ofile[10],ifile[10],temp[25];
+    cout<<"Enter the first file name :"<<endl;//Read the name of the input file
+    cin>>ofile;
+    cout<<"Enter the number of names to be entered : "<<endl;
+    cin>>n;
+    cout<<"Enter the names :"<<endl;
+    file1.open(ofile,ios :: out);
+    while(i<n){
+        cin>>temp;
+        file1<<temp<<endl;
+        i++;
+
+    }
+    file1.close();
+    file1.open(ofile,ios::in);//reopen input file in read mode
+    cout<<"Enter the second file name :"<<endl;
+    cin>>ifile;
+    file2.open(ifile,ios::out);//Open output file in write mode
+    i=0;
+    while(i<n){
+        file1>>temp;
+        file2<<strrev(temp)<<endl;
+        cout<<temp<<endl;
+        i++;
+    }
+    cout<<"Names are reversed and written successfully";
 
 }
